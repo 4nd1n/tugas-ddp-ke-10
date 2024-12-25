9deda3b65ee76ffed70891add04f9a6979538931
4nd1n <andinidini02222@gmail.com>
2024-12-25 14:36:15 +0700

    Add files via upload

---
+++ call.py
@@ -0,0 +1,23 @@
+import main
+
+main.l_persegi(10)
+main.l_persegi_panjang(2, 4)
+main.l_jajargenjang(7, 5)
+main.l_lingkaran(2)
+main.l_segitiga(12, 2)
+sz
+import bangunruang
+
+bangunruang.l_kubus(12)
+bangunruang.l_balok(2, 4, 10)
+bangunruang.l_prisma(20, 5, 8)
+bangunruang.l_tabung(3.14, 5, 9)
+bangunruang.l_kerucut(3.14, 5, 7, 9)
+
+
+import hitung
+hitung.pertambahan(89, 54)
+hitung.pengurangan(200, 5)
+hitung.perkalian(100, 50)
+hitung.pembagian(500, 5)
+hitung.pangkat(2, 3)
